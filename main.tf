d6978b9c3dc6eb8d414e0eb5bfd0c8d92c1a2ae7
seerat-sawhney <seeratcyber@gmail.com>
2025-02-07 17:28:49 -0500

    feat(terraform): Add Virtual Network Interface Card (NIC) configuration

--- main.tf
+++ main.tf
@@ -97,3 +97,14 @@ resource "azurerm_network_security_group" "example" {
     destination_address_prefix = "*"
   }
 }
+
+resource "azurerm_network_interface" "example" {
+  name                      = "${var.labelPrefix}-A05-NIC"
+  location                  = azurerm_resource_group.example.location
+  resource_group_name       = azurerm_resource_group.example.name
+  virtual_network_name      = azurerm_virtual_network.example.name
+  subnet_id                 = azurerm_subnet.example.id
+  private_ip_address_allocation = "Dynamic"
+  public_ip_address_id      = azurerm_public_ip.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
