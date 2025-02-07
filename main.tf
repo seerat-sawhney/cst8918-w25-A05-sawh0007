ed959bc9979a583bfe3fcba607be40a211aec9ce
seerat-sawhney <seeratcyber@gmail.com>
2025-02-07 17:04:26 -0500

    Defined Azure resource group and added region variable

---
+++ main.tf
@@ -0,0 +1,47 @@
+variable "labelPrefix" {
+  description = "Prefix for resource names"
+  type        = string
+}
+
+variable "region" {
+  description = "Azure region for deployment"
+  type        = string
+  default     = "East US"  # You can change this to another region if needed
+}
+
+variable "admin_username" {
+  description = "Admin username for the VM"
+  type        = string
+  default     = "azureadmin"  # Change this to whatever username you want
+}
+
+# Configure the Terraform runtime requirements.
+terraform {
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    # Azure Resource Manager provider and version
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "2.3.3"
+    }
+  }
+}
+
+# Define providers and their config params
+provider "azurerm" {
+  features {} # Required by AzureRM provider
+}
+
+provider "cloudinit" {
+  # No additional configuration required
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "sawh0007-A05-RG"
+  location = var.region
+}
